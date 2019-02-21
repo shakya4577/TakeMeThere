34fc4d7fc4aaa4ab478fe18bd511cbb73b33a09e
Wilson Kardam <wilson.kardam@globallogic.com>
2019-02-21 16:59:03 +0530

    Realm added to project

---
+++ TakeMeThere/Pods/Target Support Files/Pods-TakeMeThere/Pods-TakeMeThere-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TakeMeThere : NSObject
+@end
+@implementation PodsDummy_Pods_TakeMeThere
+@end
---
+++ TakeMeThere/TakeMeThere/Realm/UserDetailModel.swift
@@ -0,0 +1,12 @@
+//import RealmSwift
+import Realm
+import RealmSwift
+
+class UserDetail : Object
+{
+    @objc dynamic var userName = ""
+    @objc dynamic var userAddress = ""
+    @objc dynamic var userNumber = ""
+    @objc dynamic var userEmergencyNumber = ""
+}
+
