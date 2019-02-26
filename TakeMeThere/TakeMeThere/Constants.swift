09fef33a4cf69a4858b9e818248d19f0289928e8
Wilson Kardam <wilson.kardam@globallogic.com>
2019-02-26 16:04:31 +0530

    Registration flow added

--- TakeMeThere/TakeMeThere/Constants.swift
+++ TakeMeThere/TakeMeThere/Constants.swift
@@ -1,14 +1,17 @@
-//
-//  Constants.swift
-//  TakeMeThere
-//
-//  Created by Wilson Kardam on 22/02/19.
-//  Copyright © 2019 Wilson Shakya. All rights reserved.
-//
-
 import Foundation
 
 class Constants
 {
+    //User Default Keys
+    static var UserNameKey = "UserName"
+    static var UserGenderKey = "UserGender"
+    static var userEmgNoOneKey = "UserEmgNoOne"
+    static var userEmgNoTwoKey = "UserEmgNoTWo"
+    static var userEmgNoThreeKey = "UserEmgNoThree"
+    static var userEmgAddrOneKey = "UserEmgAddOne"
+    static var userEmgAddrTwoKey = "UserEmgAddTwo"
+    static var userRegisterStepCounterKey = "UserRegisterCounter"
+    static var userRegisterDone = "UserRegisterDone"
+    // Constant strings
     static var awakeMessage = "Hi! I am listening"
 }
