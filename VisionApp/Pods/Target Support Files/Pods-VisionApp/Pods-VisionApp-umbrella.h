9909237e5180d4eaa89c967b5d669dd780a6d234
Wilson Kardam <wilson.kardam@globallogic.com>
2019-03-01 18:21:05 +0530

    renmed

--- VisionApp/Pods/Target Support Files/Pods-VisionApp/Pods-VisionApp-umbrella.h
+++ VisionApp/Pods/Target Support Files/Pods-VisionApp/Pods-VisionApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_VisionAppAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VisionAppAppVersionString[];
+FOUNDATION_EXPORT double Pods_VisionAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VisionAppVersionString[];
 
--- VisionApp/Pods/Target Support Files/Pods-VisionAppApp/Pods-VisionAppApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VisionAppApp : NSObject
-@end
-@implementation PodsDummy_Pods_VisionAppApp
-@end
